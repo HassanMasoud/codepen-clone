f2ea4778e424e0bbeef0c98947498b93e8bdc5c9
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-19 23:48:29 +0300

    Add expand and collapse button

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -5,6 +5,8 @@ import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
 function Editor(props) {
   const { language, displayName, value, onChange } = props;
@@ -17,7 +19,12 @@ function Editor(props) {
     <div className={`editor-container ${open ? '' : 'collapsed'}`}>
       <div className="editor-title">
         {displayName}
-        <button onClick={() => setOpen(prevOpen => !prevOpen)}>O/C</button>
+        <button onClick={() => setOpen(prevOpen => !prevOpen)}
+          type="button"
+          className="expand-collapse-button"
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
